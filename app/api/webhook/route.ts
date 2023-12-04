2c9ffef96f4f968450f82395decea72bf37c30ec
Kingchen711 <Kingchenobama711@gmail.ocm>
2023-12-04 18:44:29 +0700

    try put api webhook in app folder

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -3,13 +3,14 @@ import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
 // import { createdUser, deleteUser, updateUser } from '@/lib/actions/user.action'
-import { createUser } from '../../lib/_actions/user'
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextResponse } from 'next/server'
+import { createUser } from '@/lib/_actions/user'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
-    return res.json({ message: 'OK' })
-  }
+export async function GET() {
+  return NextResponse.json({ message: 'ok' })
+}
+
+export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const NEXT_CLERK_WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET
 
@@ -17,16 +18,21 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     throw new Error('Please add NEXT_CLERK_WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
   }
 
-  // Get the headers.cookies
-  const { svix_id, svix_timestamp, svix_signature } = req.cookies
+  // Get the headers
+  const headerPayload = headers()
+  const svix_id = headerPayload.get('svix-id')
+  const svix_timestamp = headerPayload.get('svix-timestamp')
+  const svix_signature = headerPayload.get('svix-signature')
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return res.status(400).json({ message: 'Error occured -- no svix headers' })
+    return new Response('Error occured -- no svix headers', {
+      status: 400
+    })
   }
 
   // Get the body
-  const payload = await req.body
+  const payload = await req.json()
   const body = JSON.stringify(payload)
 
   // Create a new Svix instance with your secret.
@@ -43,7 +49,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }) as WebhookEvent
   } catch (err) {
     console.error('Error verifying webhook:', err)
-    return res.status(400).json({ message: 'Error occured' })
+    return new Response('Error occured', {
+      status: 400
+    })
   }
 
   const eventType = evt.type
@@ -59,7 +67,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       username: username!
     })
 
-    return res.json({ message: 'ok', user: mongoUser })
+    return NextResponse.json({ message: 'ok', user: mongoUser })
   }
 
   if (eventType === 'user.updated') {
@@ -76,8 +84,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     //   path: `/profile/${id}`
     // })
 
-    // return NextResponse.json({ message: 'ok', user: mongoUser })
-    return res.json({ message: 'ok' })
+    return NextResponse.json({ message: 'ok' })
   }
 
   if (eventType === 'user.deleted') {
@@ -87,9 +94,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     //   clerkId: id!
     // })
 
-    // return NextResponse.json({ message: 'ok', user: deletedUser })
-    return res.json({ message: 'ok' })
+    return NextResponse.json({ message: 'ok' })
   }
 
-  return res.json({ message: 'OK' })
+  return NextResponse.json({ message: 'OK' })
 }
