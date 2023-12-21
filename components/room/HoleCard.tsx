92ab27aa7e3c447b63924ce734a1fde278cd0ba6
Kingchen711 <Kingchenobama711@gmail.ocm>
2023-12-22 04:05:33 +0700

    try another back card

--- components/room/HoleCard.tsx
+++ components/room/HoleCard.tsx
@@ -18,7 +18,7 @@ function HoleCard({ winner, firstCard, secondCard, hidden = false, className }: 
         {winner && <div className='absolute inset-0 z-30 translate-x-[30%] rotate-12 rounded-md bg-black/50'></div>}
         <Image
           fill
-          src={hidden ? '/assets/cards/back-card.png' : firstCard && getCardImage(firstCard)!}
+          src={hidden ? '/assets/cards/back-card.jpg' : firstCard && getCardImage(firstCard)!}
           alt='first card'
           className={cn(
             'rounded-md absolute translate-x-[30%] rotate-12',
@@ -30,7 +30,7 @@ function HoleCard({ winner, firstCard, secondCard, hidden = false, className }: 
         {winner && <div className='absolute inset-0 z-30 -rotate-12 bg-black/50'></div>}
         <Image
           fill
-          src={hidden ? '/assets/cards/back-card.png' : secondCard && getCardImage(secondCard)!}
+          src={hidden ? '/assets/cards/back-card.jpg' : secondCard && getCardImage(secondCard)!}
           alt='second card'
           className={cn('rounded-md absolute -rotate-12', winner && isWinnerCard(winner, secondCard!) && 'z-50')}
         />
